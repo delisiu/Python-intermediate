eb6135d393b788fedda0a8cdc243b809b5569de1
Maciej Deliś <delisiu@gmail.com>
2020-01-06 02:10:10 +0100

    Lab 10 uncomment second solution

--- 02_zmienne i kod/LAB_10_Boolean conversion.py
+++ 02_zmienne i kod/LAB_10_Boolean conversion.py
@@ -68,30 +68,30 @@ while x:
 #     Jeśli nie:
 #         wyświetl informację o zakończeniu działania programu
 
-# def DisplayOptions(options):
-#     for i in range(len(options)):
-#         print("{} - {}".format(i+1, options[i]))
+def DisplayOptions(options):
+    for i in range(len(options)):
+        print("{} - {}".format(i+1, options[i]))
     
-#     choice = input('Select option above or press enter to exit: ')
-#     return choice
+    choice = input('Select option above or press enter to exit: ')
+    return choice
     
     
-# choice='x'
-# options = ['load data', 'export data', 'analyze & predict']
+choice='x'
+options = ['load data', 'export data', 'analyze & predict']
     
-# while choice:
+while choice:
     
-#     choice = DisplayOptions(options)
+    choice = DisplayOptions(options)
     
-#     #executed only if something was entered
-#     if choice:
-#         try:
-#             choice_num = int(choice)-1
-#             if choice_num >=0 and choice_num < len(options):
-#                 print("you have selected {} - {}".format(choice_num+1, options[choice_num]))
-#             else:
-#                 print("choose a value from a list or press enter")
-#         except:
-#             print("You need to enter a number")
-#     else:
-#         print('----- END -----')
+    #executed only if something was entered
+    if choice:
+        try:
+            choice_num = int(choice)-1
+            if choice_num >=0 and choice_num < len(options):
+                print("you have selected {} - {}".format(choice_num+1, options[choice_num]))
+            else:
+                print("choose a value from a list or press enter")
+        except:
+            print("You need to enter a number")
+    else:
+        print('----- END -----')
